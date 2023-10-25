35df410184f06dc0c759800a9cf667c3d78a1084
Richard765 <richardexposito04@gmail.com>
2023-10-25 19:52:13 +0200

    foreach

---
+++ Actividades/Actividades PHP/foreach.php
@@ -0,0 +1,8 @@
+<?php 
+$meses = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+"Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+foreach ($meses as $mes) {
+echo "$mes \n", "<br>";
+}
+?>
+
